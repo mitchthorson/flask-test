4362033f221534ed37b6183bb0f5b86d712fa057
mitchthorson <mthorson@usatoday.com>
2015-02-06 11:57:59 -0500

    small update

--- application.py
+++ application.py
@@ -6,13 +6,13 @@ tasks = [
         'id': 1,
         'title': u'Buy groceries',
         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
+        'done': True
     },
     {
         'id': 2,
         'title': u'Learn Python',
         'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
+        'done': True
     }
 ]
 
