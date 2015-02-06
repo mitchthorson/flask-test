5ed33f1c331158cda86e191b2666409b0dea54a7
mitchthorson <mthorson@usatoday.com>
2015-02-06 14:10:38 -0500

    add more methods

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort, make_response
 application = Flask(__name__)
 
 tasks = [
@@ -16,9 +16,31 @@ tasks = [
     }
 ]
 
-@application.route("/")
+@application.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@application.route("/", methods=['GET'])
 def hello():
     return jsonify({'tasks': tasks})
 
+@application.route("/add", methods=['POST'])
+def create_task():
+    print(request)
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
+
+
+
+
